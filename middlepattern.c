ca81684f443a27a27a04848eff5e9f2ec52d06c7
Deepa Subramanyam <68331226+deepasubramanyam@users.noreply.github.com>
2021-02-22 11:21:45 +0530

    middle_odd_length_string_pattern

---
+++ middlepattern.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+    int i,j,k,mid;
+    char str[100];
+    scanf("%s", str);
+    mid=strlen(str)/2;
+    for(i=1;i<=strlen(str);i++)
+    {
+        int temp=(i*2)-1,count=0;
+        for(j=2*strlen(str)-1;j>=i;j--)
+        {
+            if(abs(temp)==j)
+            {
+                for(k=temp;count<i;k++){
+                    if(count>=7)
+                        printf("%c",str[(mid+count)%10]);
+                    else
+                        printf("%c",str[(mid+count)%7]);
+                    count++;
+                }
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
